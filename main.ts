962a20582ce2778c27f86444a0591de415b6a1b3
areyouseriousrightnowbro <114748727+areyouseriousrightnowbro@users.noreply.github.com>
2023-01-12 09:53:06 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,46 @@
 namespace SpriteKind {
     export const example = SpriteKind.create()
+    export const Cursor = SpriteKind.create()
+    export const Clicker = SpriteKind.create()
+    export const Decoy = SpriteKind.create()
+    export const Master = SpriteKind.create()
 }
+function SetCursor () {
+    MyCursor = sprites.create(img`
+        ...c...............
+        ...cc..............
+        ...c1c.............
+        ...f11bb...........
+        ...f111dc..........
+        ...f11111c.........
+        ...f111111cd.......
+        ...f1111111cb......
+        ...f11111111bc.....
+        ...f111111111dc....
+        ...f11111111111c...
+        ...f111111fffccc...
+        ...fd11f11f........
+        ...fddf.f11c.......
+        ...f1f..f11c.......
+        ...ff....f11b......
+        .........fd1b......
+        ..........ffb......
+        ...................
+        `, SpriteKind.Cursor)
+    MyClicker = sprites.create(img`
+        7 7 7 
+        7 7 7 
+        7 7 7 
+        `, SpriteKind.Clicker)
+    MyClicker.setPosition(71, 51)
+    controller.moveSprite(MyCursor, 150, 150)
+    controller.moveSprite(MyClicker, 150, 150)
+    MyCursor.setStayInScreen(true)
+    MyClicker.setStayInScreen(true)
+}
+sprites.onOverlap(SpriteKind.Clicker, SpriteKind.Master, function (sprite, otherSprite) {
+    game.over(true)
+})
 function Start_Game () {
     gamejam.showInstruction("FIND!", 500)
     scene.setBackgroundImage(img`
@@ -125,7 +165,12 @@ function Start_Game () {
         fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
         fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
         `)
+    check = false
+    SpriteX = 27
+    SpriteY = 70
     Mark = randint(0, 4)
+    row = randint(0, 3)
+    RowVer = 0
     if (Mark == 0) {
         MarkedSprite = sprites.create(img`
             . . . . . . f f f f . . . . . . 
@@ -147,6 +192,7 @@ function Start_Game () {
             `, SpriteKind.example)
         MarkedSprite.setPosition(78, 37)
         MarkedSprite.scale = 2
+        Hiding = 0
     } else if (Mark == 1) {
         MarkedSprite = sprites.create(img`
             . . . . . f f 4 4 f f . . . . . 
@@ -168,6 +214,7 @@ function Start_Game () {
             `, SpriteKind.example)
         MarkedSprite.setPosition(78, 37)
         MarkedSprite.scale = 2
+        Hiding = 1
     } else if (Mark == 2) {
         MarkedSprite = sprites.create(img`
             . . . . f f f f . . . . 
@@ -189,6 +236,7 @@ function Start_Game () {
             `, SpriteKind.example)
         MarkedSprite.setPosition(78, 37)
         MarkedSprite.scale = 2
+        Hiding = 2
     } else if (Mark == 3) {
         MarkedSprite = sprites.create(img`
             . . . . . . . c c c . . . . . . 
@@ -210,6 +258,7 @@ function Start_Game () {
             `, SpriteKind.example)
         MarkedSprite.setPosition(78, 37)
         MarkedSprite.scale = 2
+        Hiding = 3
     } else if (Mark == 4) {
         MarkedSprite = sprites.create(img`
             . f f f . f f f f . f f f . 
@@ -231,26 +280,286 @@ function Start_Game () {
             `, SpriteKind.example)
         MarkedSprite.setPosition(78, 37)
         MarkedSprite.scale = 2
+        Hiding = 4
     }
 }
+function SpawnCrowd () {
+    for (let index = 0; index < 4; index++) {
+        for (let index = 0; index < 11; index++) {
+            DecoyChooser = randint(0, 5)
+            if (DecoyChooser == 0) {
+                if (Hiding == 0) {
+                    if (check == false) {
+                        if (RowVer == row) {
+                            Dcoy = sprites.create(img`
+                                . . . . . . f f f f . . . . . . 
+                                . . . . f f f 2 2 f f f . . . . 
+                                . . . f f f 2 2 2 2 f f f . . . 
+                                . . f f f e e e e e e f f f . . 
+                                . . f f e 2 2 2 2 2 2 e e f . . 
+                                . . f e 2 f f f f f f 2 e f . . 
+                                . . f f f f e e e e f f f f . . 
+                                . f f e f b f 4 4 f b f e f f . 
+                                . f e e 4 1 f d d f 1 4 e e f . 
+                                . . f e e d d d d d d e e f . . 
+                                . . . f e e 4 4 4 4 e e f . . . 
+                                . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+                                . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+                                . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+                                . . . . . f f f f f f . . . . . 
+                                . . . . . f f . . f f . . . . . 
+                                `, SpriteKind.Master)
+                            Dcoy.setPosition(SpriteX, SpriteY)
+                            SpriteX += 10
+                            check = true
+                        }
+                    }
+                } else if (Hiding != 0) {
+                    Dcoy = sprites.create(img`
+                        . . . . . . f f f f . . . . . . 
+                        . . . . f f f 2 2 f f f . . . . 
+                        . . . f f f 2 2 2 2 f f f . . . 
+                        . . f f f e e e e e e f f f . . 
+                        . . f f e 2 2 2 2 2 2 e e f . . 
+                        . . f e 2 f f f f f f 2 e f . . 
+                        . . f f f f e e e e f f f f . . 
+                        . f f e f b f 4 4 f b f e f f . 
+                        . f e e 4 1 f d d f 1 4 e e f . 
+                        . . f e e d d d d d d e e f . . 
+                        . . . f e e 4 4 4 4 e e f . . . 
+                        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+                        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+                        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+                        . . . . . f f f f f f . . . . . 
+                        . . . . . f f . . f f . . . . . 
+                        `, SpriteKind.Decoy)
+                    Dcoy.setPosition(SpriteX, SpriteY)
+                    SpriteX += 10
+                }
+            } else if (DecoyChooser == 1) {
+                if (Hiding == 1) {
+                    if (check == false) {
+                        if (RowVer == row) {
+                            Dcoy = sprites.create(img`
+                                . . . . . f f 4 4 f f . . . . . 
+                                . . . . f 5 4 5 5 4 5 f . . . . 
+                                . . . f e 4 5 5 5 5 4 e f . . . 
+                                . . f b 3 e 4 4 4 4 e 3 b f . . 
+                                . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+                                . f 3 3 e b 3 e e 3 b e 3 3 f . 
+                                . f 3 3 f f e e e e f f 3 3 f . 
+                                . f b b f b f e e f b f b b f . 
+                                . f b b e 1 f 4 4 f 1 e b b f . 
+                                f f b b f 4 4 4 4 4 4 f b b f f 
+                                f b b f f f e e e e f f f b b f 
+                                . f e e f b d d d d b f e e f . 
+                                . . e 4 c d d d d d d c 4 e . . 
+                                . . e f b d b d b d b b f e . . 
+                                . . . f f 1 d 1 d 1 d f f . . . 
+                                . . . . . f f b b f f . . . . . 
+                                `, SpriteKind.Master)
+                            Dcoy.setPosition(SpriteX, SpriteY)
+                            SpriteX += 10
+                            check = true
+                        }
+                    }
+                } else if (Hiding != 1) {
+                    Dcoy = sprites.create(img`
+                        . . . . . f f 4 4 f f . . . . . 
+                        . . . . f 5 4 5 5 4 5 f . . . . 
+                        . . . f e 4 5 5 5 5 4 e f . . . 
+                        . . f b 3 e 4 4 4 4 e 3 b f . . 
+                        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+                        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+                        . f 3 3 f f e e e e f f 3 3 f . 
+                        . f b b f b f e e f b f b b f . 
+                        . f b b e 1 f 4 4 f 1 e b b f . 
+                        f f b b f 4 4 4 4 4 4 f b b f f 
+                        f b b f f f e e e e f f f b b f 
+                        . f e e f b d d d d b f e e f . 
+                        . . e 4 c d d d d d d c 4 e . . 
+                        . . e f b d b d b d b b f e . . 
+                        . . . f f 1 d 1 d 1 d f f . . . 
+                        . . . . . f f b b f f . . . . . 
+                        `, SpriteKind.Decoy)
+                    Dcoy.setPosition(SpriteX, SpriteY)
+                    SpriteX += 10
+                }
+            } else if (DecoyChooser == 2) {
+                if (Hiding == 2) {
+                    if (check == false) {
+                        if (RowVer == row) {
+                            Dcoy = sprites.create(img`
+                                . . . . f f f f . . . . 
+                                . . f f e e e e f f . . 
+                                . f f e e e e e e f f . 
+                                f f f f 4 e e e f f f f 
+                                f f f 4 4 4 e e f f f f 
+                                f f f 4 4 4 4 e e f f f 
+                                f 4 e 4 4 4 4 4 4 e 4 f 
+                                f 4 4 f f 4 4 f f 4 4 f 
+                                f e 4 d d d d d d 4 e f 
+                                . f e d d b b d d e f . 
+                                . f f e 4 4 4 4 e f f . 
+                                e 4 f b 1 1 1 1 b f 4 e 
+                                4 d f 1 1 1 1 1 1 f d 4 
+                                4 4 f 6 6 6 6 6 6 f 4 4 
+                                . . . f f f f f f . . . 
+                                . . . f f . . f f . . . 
+                                `, SpriteKind.Master)
+                            Dcoy.setPosition(SpriteX, SpriteY)
+                            SpriteX += 10
+                            check = true
+                        }
+                    }
+                } else if (Hiding != 2) {
+                    Dcoy = sprites.create(img`
+                        . . . . f f f f . . . . 
+                        . . f f e e e e f f . . 
+                        . f f e e e e e e f f . 
+                        f f f f 4 e e e f f f f 
+                        f f f 4 4 4 e e f f f f 
+                        f f f 4 4 4 4 e e f f f 
+                        f 4 e 4 4 4 4 4 4 e 4 f 
+                        f 4 4 f f 4 4 f f 4 4 f 
+                        f e 4 d d d d d d 4 e f 
+                        . f e d d b b d d e f . 
+                        . f f e 4 4 4 4 e f f . 
+                        e 4 f b 1 1 1 1 b f 4 e 
+                        4 d f 1 1 1 1 1 1 f d 4 
+                        4 4 f 6 6 6 6 6 6 f 4 4 
+                        . . . f f f f f f . . . 
+                        . . . f f . . f f . . . 
+                        `, SpriteKind.Decoy)
+                    Dcoy.setPosition(SpriteX, SpriteY)
+                    SpriteX += 10
+                }
+            } else if (DecoyChooser == 3) {
+                if (Hiding == 3) {
+                    if (check == false) {
+                        if (RowVer == row) {
+                            Dcoy = sprites.create(img`
+                                . . . . . . . c c c . . . . . . 
+                                . . . . . . c b 5 c . . . . . . 
+                                . . . . c c c 5 5 c c c . . . . 
+                                . . c c b c 5 5 5 5 c c c c . . 
+                                . c b b 5 b 5 5 5 5 b 5 b b c . 
+                                . c b 5 5 b b 5 5 b b 5 5 b c . 
+                                . . f 5 5 5 b b b b 5 5 5 c . . 
+                                . . f f 5 5 5 5 5 5 5 5 f f . . 
+                                . . f f f b f e e f b f f f . . 
+                                . . f f f 1 f b b f 1 f f f . . 
+                                . . . f f b b b b b b f f . . . 
+                                . . . e e f e e e e f e e . . . 
+                                . . e b c b 5 b b 5 b f b e . . 
+                                . . e e f 5 5 5 5 5 5 f e e . . 
+                                . . . . c b 5 5 5 5 b c . . . . 
+                                . . . . . f f f f f f . . . . . 
+                                `, SpriteKind.Master)
+                            Dcoy.setPosition(SpriteX, SpriteY)
+                            SpriteX += 10
+                            check = true
+                        }
+                    }
+                } else if (Hiding != 3) {
+                    Dcoy = sprites.create(img`
+                        . . . . . . . c c c . . . . . . 
+                        . . . . . . c b 5 c . . . . . . 
+                        . . . . c c c 5 5 c c c . . . . 
+                        . . c c b c 5 5 5 5 c c c c . . 
+                        . c b b 5 b 5 5 5 5 b 5 b b c . 
+                        . c b 5 5 b b 5 5 b b 5 5 b c . 
+                        . . f 5 5 5 b b b b 5 5 5 c . . 
+                        . . f f 5 5 5 5 5 5 5 5 f f . . 
+                        . . f f f b f e e f b f f f . . 
+                        . . f f f 1 f b b f 1 f f f . . 
+                        . . . f f b b b b b b f f . . . 
+                        . . . e e f e e e e f e e . . . 
+                        . . e b c b 5 b b 5 b f b e . . 
+                        . . e e f 5 5 5 5 5 5 f e e . . 
+                        . . . . c b 5 5 5 5 b c . . . . 
+                        . . . . . f f f f f f . . . . . 
+                        `, SpriteKind.Decoy)
+                    Dcoy.setPosition(SpriteX, SpriteY)
+                    SpriteX += 10
+                }
+            } else if (DecoyChooser == 4) {
+                if (Hiding == 4) {
+                    if (check == false) {
+                        if (RowVer == row) {
+                            Dcoy = sprites.create(img`
+                                . f f f . f f f f . f f f . 
+                                f f f f f c c c c f f f f f 
+                                f f f f b c c c c b f f f f 
+                                f f f c 3 c c c c 3 c f f f 
+                                . f 3 3 c c c c c c 3 3 f . 
+                                . f c c c c 4 4 c c c c f . 
+                                . f f c c 4 4 4 4 c c f f . 
+                                . f f f b f 4 4 f b f f f . 
+                                . f f 4 1 f d d f 1 4 f f . 
+                                . . f f d d d d d d f f . . 
+                                . . e f e 4 4 4 4 e f e . . 
+                                . e 4 f b 3 3 3 3 b f 4 e . 
+                                . 4 d f 3 3 3 3 3 3 c d 4 . 
+                                . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+                                . . . . f f f f f f . . . . 
+                                . . . . f f . . f f . . . . 
+                                `, SpriteKind.Master)
+                            Dcoy.setPosition(SpriteX, SpriteY)
+                            SpriteX += 10
+                            check = true
+                        }
+                    }
+                } else if (Hiding != 4) {
+                    Dcoy = sprites.create(img`
+                        . f f f . f f f f . f f f . 
+                        f f f f f c c c c f f f f f 
+                        f f f f b c c c c b f f f f 
+                        f f f c 3 c c c c 3 c f f f 
+                        . f 3 3 c c c c c c 3 3 f . 
+                        . f c c c c 4 4 c c c c f . 
+                        . f f c c 4 4 4 4 c c f f . 
+                        . f f f b f 4 4 f b f f f . 
+                        . f f 4 1 f d d f 1 4 f f . 
+                        . . f f d d d d d d f f . . 
+                        . . e f e 4 4 4 4 e f e . . 
+                        . e 4 f b 3 3 3 3 b f 4 e . 
+                        . 4 d f 3 3 3 3 3 3 c d 4 . 
+                        . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+                        . . . . f f f f f f . . . . 
+                        . . . . f f . . f f . . . . 
+                        `, SpriteKind.Decoy)
+                    Dcoy.setPosition(SpriteX, SpriteY)
+                    SpriteX += 10
+                }
+            } else if (DecoyChooser == 5) {
+                SpriteX += 10
+            }
+        }
+        RowVer += 1
+        SpriteX = 27
+        SpriteY += 10
+    }
+    SpriteY += 10
+}
+// Fix Issue where master is not spawning
+let Dcoy: Sprite = null
+let DecoyChooser = 0
+let Hiding = 0
 let MarkedSprite: Sprite = null
+let RowVer = 0
+let row = 0
 let Mark = 0
+let SpriteY = 0
+let SpriteX = 0
+let check = false
+let MyClicker: Sprite = null
+let MyCursor: Sprite = null
 Start_Game()
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+SpawnCrowd()
+SetCursor()
+game.onUpdate(function () {
+    if (MyClicker.x != MyCursor.x - 9 || MyClicker.y != MyCursor.y - 9) {
+        MyClicker.setPosition(MyCursor.x - 9, MyCursor.y - 9)
+    }
+})
